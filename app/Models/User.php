23783645b4f92b0155be60073c71a65055662d15
DaniAm94 <dani.amico94@gmail.com>
2024-04-18 00:02:25 +0200

    removed un-used model importation fron user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Models\Appartment;
 
 
 class User extends Authenticatable
