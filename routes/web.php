b7334d475cb2cf9a21584f79616460df017e0c78
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-04-18 11:41:28 +0200

    add routes apartments , create ApartmentController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApartmentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('apartments', ApartmentController::class);
 
 Route::get('/admin', function () {
     return view('admin.home');
