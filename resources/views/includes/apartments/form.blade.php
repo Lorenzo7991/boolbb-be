8ff80b4149b936a436b3aaaf5b7b3ad3f9d73431
dieghino26 <brotzadiego@gmail.com>
2024-04-19 15:56:06 +0200

    added a separate file for image preview

--- resources/views/includes/apartments/form.blade.php
+++ resources/views/includes/apartments/form.blade.php
@@ -130,38 +130,5 @@
 </form>
 
 <script>
-    const placeholder = 'https://marcolanci.it/boolean/assets/placeholder.png';
-    const inputImage = document.getElementById('image');
-    const preview = document.getElementById('preview');
-    const changeImageButton = document.getElementById('change-image-btn');
-    const fakeImageField = document.getElementById('fake-image-field');
-
-    // Gestione image preview
-    let blobUrl;
-    inputImage.addEventListener('change', () => {
-
-        // Controllo se è stato selezionato un file
-        if (inputImage.files && inputImage.files[0]) {
-            // prendo il file
-            const file = inputImage.files[0];
-            // preparo un url temporaneo
-            blobUrl = URL.createObjectURL(file);
-            preview.src = blobUrl;
-        } else {
-            preview.src = placeholder;
-        }
-    })
-
-    window.addEventListener('beforeunload', () => {
-        if (blobUrl) URL.revokeObjectURL(blobUrl);
-    })
-
-    // Gestione campo file
-    // al click del bottono cambio l'input mostrato
-    changeImageButton.addEventListener('click', () => {
-        fakeImageField.classList.add('d-none');
-        inputImage.classList.remove('d-none');
-        preview.src = placeHolder;
-        inputImage.click();
-    })
+    @vite('resources/js/image_preview.js')
 </script>
