ba1313af4465bf04a233d744c2c7349d385a2215
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-19 17:24:19 +0200

    added icon urls

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -14,15 +14,15 @@ class ServiceSeeder extends Seeder
     public function run(): void
     {
         $services = [
-            ['label' => 'Wi-fi', 'icon' => 'connessione'],
-            ['label' => 'Posto Macchina', 'icon' => 'posto'],
-            ['label' => 'Garage', 'icon' => 'garage'],
-            ['label' => 'Piscina', 'icon' => 'piscina'],
-            ['label' => 'Portineria', 'icon' => 'porta'],
-            ['label' => 'Sauna', 'icon' => 'sauna'],
-            ['label' => 'Vista Mare', 'icon' => 'mare'],
-            ['label' => 'Cucina', 'icon' => 'cucina'],
-            ['label' => 'Aria Condizionata', 'icon' => 'aria'],
+            ['label' => 'Wi-fi', 'icon' => 'https://w7.pngwing.com/pngs/488/305/png-transparent-black-and-white-wifi-wi-fi-computer-icons-wireless-symbol-wifi-miscellaneous-computer-network-mobile-phones-thumbnail.png'],
+            ['label' => 'Posto Macchina', 'icon' => 'https://cdn-icons-png.flaticon.com/512/8/8206.png'],
+            ['label' => 'Garage', 'icon' => 'https://w7.pngwing.com/pngs/387/328/png-transparent-car-garage-computer-icons-garage-angle-text-rectangle-thumbnail.png'],
+            ['label' => 'Piscina', 'icon' => 'https://cdn-icons-png.flaticon.com/512/157/157839.png'],
+            ['label' => 'Portineria', 'icon' => 'https://cdn-icons-png.flaticon.com/512/1060/1060203.png'],
+            ['label' => 'Sauna', 'icon' => 'https://cdn-icons-png.flaticon.com/512/273/273395.png'],
+            ['label' => 'Vista Mare', 'icon' => 'https://static.thenounproject.com/png/2024103-200.png'],
+            ['label' => 'Cucina', 'icon' => 'https://cdn-icons-png.flaticon.com/512/72/72118.png'],
+            ['label' => 'Aria Condizionata', 'icon' => 'https://cdn-icons-png.flaticon.com/512/114/114735.png'],
         ];
 
         foreach ($services as $service) {
