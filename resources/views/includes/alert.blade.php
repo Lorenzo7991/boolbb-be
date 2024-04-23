1f3bff00b76685a66e5ce667be63aebcbacd1f7f
DaniAm94 <dani.amico94@gmail.com>
2024-04-23 17:35:22 +0200

    added alert to show errors

--- resources/views/includes/alert.blade.php
+++ resources/views/includes/alert.blade.php
@@ -1,6 +1,19 @@
+{{-- Alert per i messaggi --}}
 @session('message')
     <div class="alert alert-{{ session('type', 'info') }} alert-dismissible fade show" role="alert">
         {{ $value }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endsession
+
+{{-- Alert di validazione form --}}
+@if ($errors->any())
+    <div class="alert alert-danger mt-3">
+        <h4>Ci sono dei campi non validi!</h4>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
