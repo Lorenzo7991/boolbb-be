50b012a58480b187c1a08ba9896c49f1b405a415
DaniAm94 <dani.amico94@gmail.com>
2024-04-24 16:53:04 +0200

    removed error list from alert

--- resources/views/includes/alert.blade.php
+++ resources/views/includes/alert.blade.php
@@ -10,10 +10,5 @@
 @if ($errors->any())
     <div class="alert alert-danger mt-3">
         <h4>Ci sono dei campi non validi!</h4>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
     </div>
 @endif
