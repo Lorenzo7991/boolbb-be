d4c13a60e2363c41cf6c7569c83df960b55df1cd
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-05-02 15:12:54 +0200

    added draft layout of messages section in home view

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
+     <h2 class="home-title fs-1 fw-bolder mt-5 py-2">
+                            {{ __('Dashboard') }}
+                        </h2>
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
@@ -18,9 +18,38 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                  
+                </div>
+                
+            </div>
+             <h2 class="home-title fs-1 fw-bolder mt-3 py-2">
+                            {{ __('Messaggi') }}
+                        </h2>
+              <table class="table mt-3">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="text-center text-white brd-left"><i class=" ps-1 fa-solid fa-camera-retro icon-border"></i></th>
+                                <th scope="col" class="text-white"><i class="fa-solid fa-tag me-2  icon-border"></i>Appartamento</th>
+                                <th scope="col" class="text-white"><i class="fa-solid fa-location-dot  me-2  icon-border"></i>Indirizzo</th>
+                                <th scope="col" class="text-white"><i class="fa-solid fa-paper-plane  me-2  icon-border"></i>Mittente</th>
+                                <th scope="col" class="text-white"><i class="fa-solid fa-envelope-open  me-2  icon-border"></i>Oggetto</th>
+                                <th scope="col" class="text-white"><i class="fa-solid fa-eye  me-2  icon-border"></i>Anteprima</th>
+                                <th class="text-center text-white brd-right" scope="col"><i class="fa-solid fa-gamepad icon-border"></i>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        
+                            <tr>
+                            <td colspan="7" class="text-center">
+                                <span>Non ci sono nuovi messaggi</span>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+        @endsection
+                    
+                    
