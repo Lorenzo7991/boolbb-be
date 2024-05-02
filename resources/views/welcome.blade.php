b28a20947363275b47881cbe6a6493fb041442e7
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-05-02 12:22:20 +0200

    fixed welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 @section('content')
-    <img class="img-fluid" src="{{ asset('assets/stonks.png') }}" alt="logo">
-    <h1>STONKSBNB</h1>
+    {{-- <img class="img-fluid" src="{{ asset('assets/stonks.png') }}" alt="logo">
+    <h1>STONKSBNB</h1> --}}
 @endsection
