2c64e1ebdace50432d59653c136de32df076c69b
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-05-06 11:54:28 +0200

    changed services icons

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -14,15 +14,15 @@ class ServiceSeeder extends Seeder
     public function run(): void
     {
         $services = [
-            ['label' => 'Wi-fi', 'icon' => 'https://w7.pngwing.com/pngs/488/305/png-transparent-black-and-white-wifi-wi-fi-computer-icons-wireless-symbol-wifi-miscellaneous-computer-network-mobile-phones-thumbnail.png'],
-            ['label' => 'Posto Macchina', 'icon' => 'https://cdn-icons-png.flaticon.com/512/8/8206.png'],
-            ['label' => 'Garage', 'icon' => 'https://w7.pngwing.com/pngs/387/328/png-transparent-car-garage-computer-icons-garage-angle-text-rectangle-thumbnail.png'],
-            ['label' => 'Piscina', 'icon' => 'https://cdn-icons-png.flaticon.com/512/157/157839.png'],
-            ['label' => 'Portineria', 'icon' => 'https://cdn-icons-png.flaticon.com/512/1060/1060203.png'],
-            ['label' => 'Sauna', 'icon' => 'https://cdn-icons-png.flaticon.com/512/273/273395.png'],
-            ['label' => 'Vista Mare', 'icon' => 'https://static.thenounproject.com/png/2024103-200.png'],
-            ['label' => 'Cucina', 'icon' => 'https://cdn-icons-png.flaticon.com/512/72/72118.png'],
-            ['label' => 'Aria Condizionata', 'icon' => 'https://cdn-icons-png.flaticon.com/512/114/114735.png'],
+            ['label' => 'Wi-fi', 'icon' => 'fa-solid fa-wifi'],
+            ['label' => 'Posto Macchina', 'icon' => 'fa-solid fa-square-parking'],
+            ['label' => 'Garage', 'icon' => 'fa-solid fa-warehouse'],
+            ['label' => 'Piscina', 'icon' => 'fa-solid fa-water-ladder'],
+            ['label' => 'Portineria', 'icon' => 'fa-solid fa-bell-concierge'],
+            ['label' => 'Sauna', 'icon' => 'fa-solid fa-temperature-arrow-up'],
+            ['label' => 'Vista Mare', 'icon' => 'fa-solid fa-water'],
+            ['label' => 'Cucina', 'icon' => 'fa-solid fa-kitchen-set'],
+            ['label' => 'Aria Condizionata', 'icon' => 'fa-solid fa-wind'],
         ];
 
         foreach ($services as $service) {
