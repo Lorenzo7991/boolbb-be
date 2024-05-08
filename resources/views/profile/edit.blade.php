2a07d224d5843066e4cf2a81ddbafa9f45d89218
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-05-08 16:04:59 +0200

    add a bit of graphic on dashboard blade

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h2 class="fs-4 text-secondary my-4">
-            {{ __('Profile') }}
+        <h2 class="fs-1 fw-bolder text-center home-title my-4">
+            Opzioni Profilo: {{ Auth::user()->name }}
         </h2>
         <div class="card p-4 mb-4 bg-white shadow rounded-lg">
 
